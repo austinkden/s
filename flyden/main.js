a48c270535bf63a9e40725b0e4e994811fe8b8bc
austin <167563184+austinkden@users.noreply.github.com>
2025-08-19 19:43:13 -0600

    Create flyden's main.js

---
+++ flyden/main.js
@@ -0,0 +1,45 @@
+const startButton = document.querySelector("section.welcome button.start");
+
+startButton.addEventListener("click", () => {
+   startButton.classList.add("moving");
+   document.querySelector("section.welcome").classList.add("leaving");
+   setTimeout(() => {
+      document.querySelector("section.welcome").remove();
+      setTimeout(() => {
+         document.querySelector("section.airline").classList.remove("gone");
+         setTimeout(() => {
+            document.querySelector("section.airline").classList.remove("hidden");
+         }, 25);
+      }, 25);
+   }, 400);
+});
+
+const airlineListItems = document.querySelectorAll("section.airline div.list div.airline");
+
+airlineListItems.forEach(airline => {
+   airline.addEventListener("click", () => {
+      if (airline.classList.contains("selected")) {
+         document.querySelector("section.airline button.next").classList.remove("allowed");
+         airline.classList.remove("selected");
+      } else {
+         document.querySelectorAll("section.airline div.list div.airline").forEach(airline => {
+            airline.classList.remove("selected");
+         });
+
+         airline.classList.add("selected");
+         document.querySelector("section.airline button.next").classList.add("allowed");
+      }
+   });
+});
+
+const airlineSearch = document.querySelector("section.airline input.search");
+
+airlineSearch.addEventListener("input", () => {
+   document.querySelectorAll("section.airline div.list div.airline").forEach(airline => {
+      if (airline.querySelector("p").textContent.toLowerCase().includes(airlineSearch.value.toLowerCase())) {
+         airline.classList.remove("hidden");
+      } else {
+         airline.classList.add("hidden");
+      }
+   });
+});
